a04265e76e6295e160d97edaf0ed53e8a8af1ba9
Estalisium <131347532+Estalisium@users.noreply.github.com>
2023-04-21 22:28:40 +0200

    Update Plateau.py

--- Projet info/Plateau.py
+++ Projet info/Plateau.py
@@ -10,49 +10,36 @@ class Plateau(np.ndarray):
 
     def __init__(self, shape):
         for i in range(10):
-            if i < 5:
-                self[0][i % 5] = np.array([i, Plateau.marqueur_vide, Plateau.marqueur_vide])
-            else:
-                self[1][i % 5] = np.array([i, Plateau.marqueur_vide, Plateau.marqueur_vide])
+            self[i] = np.array([i, Plateau.marqueur_vide, Plateau.marqueur_vide])
 
     def placer_joueur(self, pos):
         if pos > 9:
             raise ValueError
-        test = 0
-        i = 0
-        while test == 0:
-            if i < 5:
-                if self[0][i % 5][0] == pos:
-                    self[0][i % 5][1] += 1
-                    test = 1
-            else:
-                if self[1][i % 5][0] == pos:
-                    self[1][i % 5][1] += 1
-                    test = 1
-            i += 1
+        for i in range(10):
+            if i == pos:
+                self[i][1] += 1
 
     def placer_creature(self, pos, type):
-        if pos > 9:
+        """
+        :type type: int, marqueur_creature = 1, marqueur_artemia = 2, marqueur_traque = 3,
+        :type pos: int
+        """
+        if pos > 9 or pos < 0:
             raise ValueError
-        test = 0
-        i = 0
-        while test == 0:
-            if i < 5:
-                if self[0][i % 5][0] == pos:
-                    self[0][i % 5][2] = type
-                    test = 1
-            else:
-                if self[1][i % 5][0] == pos:
-                    self[1][i % 5][2] = type
-                    test = 1
-            i += 1
+        for i in range(10):
+            if i == pos:
+                self[i][2] = type
 
+    def reset(self):
+        for i in range(10):
+            self[i] = np.array([i, Plateau.marqueur_vide, Plateau.marqueur_vide])
 
 
 if __name__ == '__main__':
-    test = Plateau((2, 5, 3))
+    test = Plateau((10, 3))
     print(test)
     test.placer_joueur(5)
     test.placer_joueur(5)
     test.placer_joueur(3)
+    test.placer_creature(5, 2)
     print(test)
